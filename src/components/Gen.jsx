a4397a6fe5ebf7169cf30575d9de17557317847f
User <shrihar.9919an@gmail.com>
2024-09-10 12:52:10 +0530

    done deal

--- src/components/Gen.jsx
+++ src/components/Gen.jsx
@@ -2,6 +2,7 @@ import '../index.css';
 import img from '../assets/upload.png'
 import { useEffect, useRef, useState } from 'react';
 import {Client} from '@gradio/client';
+import 'react';
 
 function Gen(){
  
@@ -34,13 +35,38 @@ const result = await client.predict("/enhance_image", {
 console.log(result.data);
 
    }
+   const handledrag=(e)=>{
+    e.preventDefault();
+   };
+   const handledrop=(e)=>{
+    console.log("dropped");
+    e.preventDefault();
+    const file=e.dataTransfer.files[0];
+    if(file){
+      if(fileRef.current){
+        const dtrans= new DataTransfer();
+        dtrans.items.add(file);
+        fileRef.current.files=dtrans.files;
+        
+      }
+    }
+   }
+
 
   return(
      <div className='bg-black h-[91vh] pt-3'>
       <h1 className='text-white text-xl mt-6 font-semibold ml-5'>PSR Image Enhancement</h1>
-      <h2 className='text-white text-lg mt-2 font-normal ml-5'>Upload an Image of the Permanently Shadowed Region(PSR) to enhance its visibility</h2>
-      <div className='border border-gray-400 w-[90vw] h-[40vh] rounded-md  mt-4  flex flex-col justify-center items-center mx-auto'>
-      <svg width="40px" height="40px" viewBox="0 -2 30 30" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+      <div className=' mx-auto w-[97vw] h-[6vh] mt-1 mb-4 flex items-center justify-between '>
+      <h2 className='inline-block text-white text-lg font-normal'>Upload an Image of the Permanently Shadowed Region(PSR) to enhance its visibility</h2>
+      {btn &&
+        <>
+      <button onClick={fetchdata} className=' inline-block bg-cyan-500 w-[30vw] text-black ease-in duration-150 hover:bg-cyan-700 rounded-full border border-transparent py-2 font-semibold'>Generate Image</button>
+        </>
+        }
+        </div>
+      <div className=' mt-2 w-[97vw] mx-auto flex justify-between'>
+      <div onDragOver={handledrag} onDrop={handledrop} onClick={handleclick}className='cursor-pointer  border border-gray-100  min-w-[48vw] min-h-[40vh] rounded-sm  py-2 flex flex-col  justify-center items-center '>
+      <svg width="35px" height="35px" viewBox="0 -2 30 30" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
     <g id="Page-1" stroke="none" stroke-width="0.5" fill="none" fill-rule="evenodd" sketch:type="MSPage">
         <g id="Icon-Set" sketch:type="MSLayerGroup" transform="translate(-569.000000, -674.000000)" fill="white">
             <path d="M579.732,681.7 L583,677.74 L583,691.998 C583,692.552 583.447,693 584,693 C584.553,693 585,692.552 585,691.998 L585,677.702 L588.299,681.7 C588.69,682.095 589.326,682.095 589.719,681.7 C590.11,681.307 590.11,680.668 589.719,680.274 L584.776,674.283 C584.566,674.073 584.289,673.983 584.016,673.998 C583.742,673.983 583.465,674.073 583.256,674.283 L578.313,680.274 C577.921,680.668 577.921,681.307 578.313,681.7 C578.705,682.095 579.341,682.095 579.732,681.7 L579.732,681.7 Z M598,690 C597.447,690 597,690.448 597,691 L597,698 L571,698 L571,691 C571,690.448 570.553,690 570,690 C569.447,690 569,690.448 569,691 L569,699 C569,699.553 569.447,700 570,700 L598,700 C598.553,700 599,699.553 599,699 L599,691 C599,690.448 598.553,690 598,690 L598,690 Z" id="upload" sketch:type="MSShapeGroup">
@@ -49,20 +75,18 @@ console.log(result.data);
         </g>
     </g>
 </svg>
-      
-      <h2 className='text-white font-light  text-lg mt-2 min-w-min'>Drag and Drop</h2>
-      <h2 className='text-white text-lg font-light'>Or</h2>
-      <button onClick={handleclick} className='w-max text-black font-semibold h-max py-1.5 px-10 rounded-full bg-white ease-in-out duration-300   hover:px-16 active:duration-0   active:font-bold mt-4'>Browse Files</button>
+      <h1 className='text-white text-lg font-Montserrat'>Drop Image here</h1>
+      <h1 className='text-gray-100 text-lg font-Montserrat'>-Or-</h1>      
+      <h1 className='text-white text-lg font-Montserrat'>Click to Upload</h1>
       <input type='file' ref={fileRef} onChange={handlechange} className='hidden'></input>
-      
-
       </div>
-       {btn &&
-        <>
-        <h2 className='text-white w-max mx-auto font-semibold text-lg' >File Chosen:{`${file}`}</h2>
-      <button onClick={fetchdata} className=' block bg-cyan-500 w-[80vw] text-black ease-in duration-150 hover:bg-cyan-700 mx-auto rounded-full border border-transparent p-2 mt-3'>Generate Image</button>
-        </>
-        }
+      <div className='border border-gray-100 min-w-[48vw] min-h-[40vh] rounded-sm  py-2 flex  justify-evenly items-center '>
+      <h1 className='text-black bg-white font-bold border-2 border-black p-2 px-4 rounded-full font-Montserrat'>Enhanced Image</h1>
+      </div>
+
+      
+        </div>
+    
 
      </div>
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,8 +22,8 @@ function Home(){
     return(
    <div className={`bg-[url('https://i.pinimg.com/originals/2a/14/78/2a1478816ebf07115dc53eddcda84450.jpg')] bg-cover bg-center h-[91vh]  flex flex-col justify-center items-center`}>
    <div className=' flex flex-col  mx-auto w-max h-max  p-5'>
-    <h1 className=' font-bold text-white font-Montserrat text-4xl'>LOW LIGHT IMAGE ENHANCEMENT</h1>
-    <h1 className='font-bold block mx-auto text-white font-Montserrat text-4xl'><i>of</i> PSR IMAGES</h1>
+    <h1 className=' font-bold text-white font-Montserrat text-4xl animate-fadeIn'>LOW LIGHT IMAGE ENHANCEMENT</h1>
+    <h1 className='font-bold block mx-auto text-white font-Montserrat text-4xl animate-fadeIn'><i>of</i> PSR IMAGES</h1>
    </div>
    <button  ref={clickRef} onClick={goto} className='block text-white border-0 border-transparent  font-Montserrat font-semibold text-lg mt-[20vh] rounded-full mx-auto p-2 px-14  transition-colors duration-300 bg-black hover:bg-white hover:text-black active:font-bold'>GET STARTED</button>
    </div>
